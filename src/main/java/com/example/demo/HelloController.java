0660fbfdde35b48838ce920fece056cdb832ae43
muvvaladeepak <muvaladeepak814@gmail.com>
2022-04-27 17:02:36 +0530

    27-04-22 2nd

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,7 +1,13 @@
 package com.example.demo;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.TreeMap;
 
 @RestController
 
@@ -11,6 +17,31 @@ public class HelloController {
 	String hello() {
 		return "Hello from my spring boot application!";
 	}
+	
+	@RequestMapping("/tools")
 
+	String tools() {
+		return "Hello from my spring boot tools application!";
+	}
+	
+	@RequestMapping("/caliculator/{first_value}/{second_value}")
+
+	String caliculator(@PathVariable Double first_value,@PathVariable Double second_value) {
+		double a = first_value+second_value;
+		double b = first_value-second_value;
+		double c = first_value*second_value;
+		double d = first_value/second_value;
+		return "addition =" +a +"subraction ="+b +" multiplication ="+c+"division ="+d;
+	}
+	
+	
+	@RequestMapping("/student/{name}/{id}/{cmarks}/{pmarks}/{mmarks}")
+	String myStudents(@PathVariable String name, @PathVariable int id, @PathVariable int cmarks, @PathVariable int pmarks, @PathVariable int mmarks) {
+		Student s = new Student(id,name,pmarks,cmarks,mmarks);
+		String grade = s.getGrade();
+		return "Hello, My name is " +name + " and my id is " + id + " and my grade is " + grade; 
+	}
+	
 
 }
+
