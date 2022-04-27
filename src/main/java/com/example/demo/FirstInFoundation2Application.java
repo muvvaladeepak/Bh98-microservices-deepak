3836fbadf60e6c6bfa0a5a8b422079977e2bed69
muvvaladeepak <muvaladeepak814@gmail.com>
2022-04-27 13:23:40 +0530

    27-04-22

---
+++ src/main/java/com/example/demo/FirstInFoundation2Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstInFoundation2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstInFoundation2Application.class, args);
+		
+
+	}
+
+}
